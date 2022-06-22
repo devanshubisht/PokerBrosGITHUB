9c8b3b66bee84367c18a60924c9fd042117306aa
devanshubisht <devanshubisht.2000@gmail.com>
2022-06-22 17:50:29 +0800

    changes

---
+++ PokerBrosV2/App Functions/PotOdds/PotOddsPage.swift
@@ -0,0 +1,32 @@
+//
+//  PotOddsPage.swift
+//  PokerBrosV2
+//
+//  Created by Devanshu Bisht on 21/6/22.
+//
+
+import UIKit
+
+class PotOddsPage: UIViewController {
+
+    @IBOutlet weak var BackButton: UIButton!
+    @IBOutlet weak var PotOddsCalc: UIButton!
+    @IBOutlet weak var PotOddsIntro: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
